09e4ad43314ed115d15fbc5adda8fe06c63cfa97
Jesse Hallett <hallettj@gmail.com>
2011-11-21 20:35:09 -0800

    basic survival instincts
    Ants avoid running into each other in addition to avoiding running into
    water.

--- MyBot.scala
+++ MyBot.scala
@@ -4,21 +4,73 @@ object MyBot extends App {
 
 class MyBot extends Bot {
 
-  def ordersFrom(game: Game): Set[Order] = {
+  type Move = Set[Order]
+  val directions = List(North, East, South, West)
 
-    // Your logic goes here.
-    // for example ...
+  val lostAntCost = 100
 
-    val directions = List(North, East, South, West)
-    val ants = game.board.myAnts.values
-    ants.flatMap{ant =>
-      // for this ant, find the first direction which is not water, if any
-      val direction = directions.find{aim =>
-        val targetTile = game.tile(aim).of(ant.tile)
-        !game.board.water.contains(targetTile)
-      }
-      // convert this (possible) direction into an order for this ant
-      direction.map{d => Order(ant.tile, d)}
-    }.toSet
+  def ordersFrom(game: Game): Move = {
+    bestMove(game, Set.empty, game.board.myAnts.values.toList)
   }
+
+  def bestMove(game: Game, partialMove: Move, pendingAnts: List[MyAnt]): Move = pendingAnts match {
+    case ant :: remainingAnts => {
+      val possibleMoves = for {
+        direction <- directions
+        possibleMove = partialMove + Order(ant.tile, direction)
+        // partialScore = partialScoreOf(game, possibleMove)
+        // if partialScore > lowScoreThreshold
+      } yield {
+        val completeMove = bestMove(game, possibleMove, remainingAnts)
+        val score = scoreOf(game, completeMove)
+        ScoredMove(completeMove, score)
+      }
+
+      val bestScoredMove = (ScoredMove.empty /: possibleMoves) { (bestMove, possibleMove) =>
+        if (possibleMove.score > bestMove.score)
+          possibleMove
+        else
+          bestMove
+      }
+
+      bestScoredMove.move
+    }
+    case Nil => partialMove
+  }
+
+  def scoreOf(game: Game, move: Move): Int = {
+    0 + lostAntCosts(game, move)
+  }
+
+  def lostAntCosts(game: Game, move: Move): Int = {
+    val positions = positionsAfter(game, move)
+
+    (0 /: positions) { (score, position) => 
+      val (tile, ants) = position
+
+      if (ants.size > 1)
+        score - lostAntCost
+      else if (game.board.water.contains(tile))
+        score - lostAntCost
+      else if (game.board.myHills.contains(tile))
+        score - lostAntCost
+      else
+        score
+    }
+  }
+
+  def positionsAfter(game: Game, move: Move): Map[Tile, Iterable[Positionable]] = {
+    (Map.empty[Tile, List[Positionable]] /: move) { (positions, order) =>
+      val antPos = order.tile
+      val newPos = game.tile(order.point).of(order.tile)
+      val existing = positions.getOrElse(newPos, List.empty[Positionable])
+      positions.updated(newPos, MyAnt(newPos) :: existing)
+    }
+  }
+
+  case class ScoredMove(val move: Move, val score: Int)
+  object ScoredMove {
+    val empty: ScoredMove = new ScoredMove(Set.empty, -99999)
+  }
+
 }
